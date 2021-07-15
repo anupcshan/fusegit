090343e1706754fa3520e2e0efc8323a4b07050b
Anup Chenthamarakshan <anupcshan@gmail.com>
2021-07-14 22:14:58 -0700

    Fix reading from overlay file

--- fusegit/overlay_file.go
+++ fusegit/overlay_file.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"path/filepath"
 	"syscall"
+	"time"
 
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
@@ -40,7 +41,23 @@ func (f *overlayFile) Setattr(ctx context.Context, fh fs.FileHandle, in *fuse.Se
 	return fs.NewLoopbackFile(fd).(fs.FileSetattrer).Setattr(ctx, in, out)
 }
 
+func (f *overlayFile) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	if fh != nil {
+		return fh.(fs.FileGetattrer).Getattr(ctx, out)
+	}
+
+	var st syscall.Stat_t
+	if err := syscall.Lstat(f.fullPath(), &st); err != nil {
+		return err.(syscall.Errno)
+	}
+	out.FromStat(&st)
+	out.SetTimeout(DefaultCacheTimeout)
+	return 0
+}
+
 func (f *overlayFile) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+	defer printTimeSince("Open", time.Now())
+
 	fd, err := syscall.Open(f.fullPath(), int(flags), 0)
 	if err != nil {
 		log.Println("Error opening", err)
